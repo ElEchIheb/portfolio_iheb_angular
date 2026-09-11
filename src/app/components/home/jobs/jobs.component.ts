7c31f6cfe20dea9c6eba9297d96080421afab2d5
El Ech Iheb <ihebelleuch6@gmail.com>
2026-09-11 05:23:11 +0100

    Split Experience/Education timelines, replace logo and favicon

--- src/app/components/home/jobs/jobs.component.ts
+++ src/app/components/home/jobs/jobs.component.ts
@@ -1,3 +1,6 @@
 import { Component } from '@angular/core';
 @Component({ selector: 'app-jobs', templateUrl: './jobs.component.html', styleUrls: ['./jobs.component.scss'], standalone: false })
-export class JobsComponent {}
+export class JobsComponent {
+  activeTab: 'work' | 'edu' = 'work';
+  setTab(tab: 'work' | 'edu'): void { this.activeTab = tab; }
+}
